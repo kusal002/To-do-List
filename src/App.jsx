9b524e0e74e99d9b8c26defaaadeef574f1836bc
kusal002 <systematicshift002@gmail.com>
2024-05-30 19:33:59 +0530

    spelling check

--- src/App.jsx
+++ src/App.jsx
@@ -75,9 +75,9 @@ function App() {
     <>
      <Navbar/> 
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-200 min-h-[80vh] md:w-[35%]">
-        <h1 className='font-bold text-center text-3xl'>Task - Manage your todos at one place</h1>
+        <h1 className='font-bold text-center text-3xl'>Task - Manage Your To-Dos At One place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
-          <h2 className='text-2xl font-bold'>Add a Todo</h2>
+          <h2 className='text-2xl font-bold'>Add a To-do</h2>
           <div className="flex">
 
           <input  onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-5 py-1' />
