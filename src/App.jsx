102588b01b8e5d154ebcd57f279d8176095911ae
kusal002 <systematicshift002@gmail.com>
2024-05-30 19:32:35 +0530

    new chnge

--- src/App.jsx
+++ src/App.jsx
@@ -74,8 +74,8 @@ function App() {
   return (
     <>
      <Navbar/> 
-       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
-        <h1 className='font-bold text-center text-3xl'>iTask - Manage your todos at one place</h1>
+       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-200 min-h-[80vh] md:w-[35%]">
+        <h1 className='font-bold text-center text-3xl'>Task - Manage your todos at one place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-2xl font-bold'>Add a Todo</h2>
           <div className="flex">
